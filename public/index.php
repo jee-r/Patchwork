1e2b194289f441cb039e940f99c42555da9e1224
jee <jee@jeer.fr>
2023-11-17 21:19:35 +0100

    feat: add footer

--- public/index.php
+++ public/index.php
@@ -7,18 +7,19 @@
     <meta charset="utf-8" />
     <meta name="description" content="A tool that generates a patchwork, an image, based on the covers of your Last.fm top albums. It's simple, free, and it works." />
     <meta name="keywords" content="lastfm top albums generator, last.fm top albums generator, lastfm top albums, last.fm top albums, lastfm, last.fm, top albums" />
-    <link href="/styles/main.css" rel="stylesheet" type="text/css" />
     <link href="/styles/pico.min.css" rel="stylesheet" type="text/css" />
+    <link href="/styles/main.css" rel="stylesheet" type="text/css" />
     <script src="/scripts/main.js"></script>
 </head>
+
 <body>
-    <nav class="container-fluid">
+    <nav>
         <ul>
-            <li><strong>Top Album Patchwork</strong></li>
+            <li><strong>Top Album <span class="lov3">Patchwork</span></strong></li>
         </ul>
         <ul>
             <li>
-                <a class="contrast" href="https://github.com" role="button">
+                <a class="contrast" href="https://github.com/jee-r/Patchwork" role="button">
                     Fork Me
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M2.6 10.59L8.38 4.8l1.69 1.7c-.24.85.15 1.78.93 2.23v5.54c-.6.34-1 .99-1 1.73a2 2 0 0 0 2 2a2 2 0 0 0 2-2c0-.74-.4-1.39-1-1.73V9.41l2.07 2.09c-.07.15-.07.32-.07.5a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2c-.18 0-.35 0-.5.07L13.93 7.5a1.98 1.98 0 0 0-1.15-2.34c-.43-.16-.88-.2-1.28-.09L9.8 3.38l.79-.78c.78-.79 2.04-.79 2.82 0l7.99 7.99c.79.78.79 2.04 0 2.82l-7.99 7.99c-.78.79-2.04.79-2.82 0L2.6 13.41c-.79-.78-.79-2.04 0-2.82Z" />
@@ -116,7 +117,10 @@
             </div>
         </article>
     </main>
-<footer>Coucou</footer>
+    <footer>
+        <div>made with <span class="lov3"><3</span></div>
+        <div>by <a href="https://artz.dev" target="_blank" rel="noopener noreferrer">Jee</a></div>
+</footer>
 </body>
 
 </html>
