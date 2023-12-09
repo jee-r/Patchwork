8c3e22cef72f6b0214d2b406bde24a461b73e7c4
jee <jee@jeer.fr>
2023-12-09 20:26:34 +0100

    style: join article

--- public/index.php
+++ public/index.php
@@ -22,7 +22,14 @@
     <nav>
         <ul>
             <li class="logo">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g id="SvgjsG1004" transform="translate(.252 .236)"><rect id="SvgjsRect1003" width="8.019" height="8.035" x="0" y="0" rx="0" ry=".772" style="display:inline;fill:#e41c61;fill-opacity:1;stroke-width:.75"/><rect id="SvgjsRect1002" width="8.019" height="8.035" x="0" y="8.635" rx="0" ry=".772" style="display:inline;fill:#191919;fill-opacity:1;stroke-width:.75"/><rect id="SvgjsRect1001" width="8.019" height="8.035" x="8.619" y="0" rx="0" ry=".772" style="display:inline;fill:#191919;fill-opacity:1;stroke-width:.75"/><rect id="SvgjsRect1000" width="8.019" height="8.035" x="8.619" y="8.635" rx="0" ry=".772" style="display:inline;fill:#e41c61;stroke-width:.75"/></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                    <g id="SvgjsG1004" transform="translate(.252 .236)">
+                        <rect id="SvgjsRect1003" width="8.019" height="8.035" x="0" y="0" rx="0" ry=".772" style="display:inline;fill:#e41c61;fill-opacity:1;stroke-width:.75" />
+                        <rect id="SvgjsRect1002" width="8.019" height="8.035" x="0" y="8.635" rx="0" ry=".772" style="display:inline;fill:#191919;fill-opacity:1;stroke-width:.75" />
+                        <rect id="SvgjsRect1001" width="8.019" height="8.035" x="8.619" y="0" rx="0" ry=".772" style="display:inline;fill:#191919;fill-opacity:1;stroke-width:.75" />
+                        <rect id="SvgjsRect1000" width="8.019" height="8.035" x="8.619" y="8.635" rx="0" ry=".772" style="display:inline;fill:#e41c61;stroke-width:.75" />
+                    </g>
+                </svg>
             </li>
             <li><strong>Top Album <span class="lov3">Patchwork</span></strong></li>
         </ul>
@@ -94,40 +101,42 @@
                 <button id="submitbtn" aria-invalid="true" type="submit">Generate</button>
             </form>
             <input id="messagebox" class="hidden" type="text" readonly>
-        </article>
 
-        <article class="hidden" id="resultcontainer">
-            <h2 id="patchworkTitle"></h2>
-            <div class="patchwork">
-                <img src="" id="patchworkImg" width="" height="" alt="Patchwork">
-            </div>
-            <div class="">
-                <div class="field-title">Dynamyc Image link :</div>
-                <div class="img-link">
-                    <a id="patchworkDynLink" href="" target="_blank"></a>
+            <!-- generated Patchwork goes here  -->
+
+            <div class="hidden" id="resultcontainer">
+                <h2 id="patchworkTitle"></h2>
+                <div class="patchwork">
+                    <img src="" id="patchworkImg" width="" height="" alt="Patchwork">
                 </div>
-                <div role="button" class="outline contrast" onclick="copyToClipboard(event, 'patchworkDynLink')">Copy</div>
-            </div>
-            <div>
-                <div class="field-title">Static Image link :</div>
-                <div class="img-link">
-                    <a id="patchworkStaticLink" href="" target="_blank"></a>
+                <div class="">
+                    <div class="field-title">Dynamyc Image link :</div>
+                    <div class="img-link">
+                        <a id="patchworkDynLink" href="" target="_blank"></a>
+                    </div>
+                    <div role="button" class="outline contrast" onclick="copyToClipboard(event, 'patchworkDynLink')">Copy</div>
                 </div>
-                <div role="button" class="outline contrast" onclick="copyToClipboard(event, 'patchworkStaticLink')">Copy</div>
+                <div>
+                    <div class="field-title">Static Image link :</div>
+                    <div class="img-link">
+                        <a id="patchworkStaticLink" href="" target="_blank"></a>
+                    </div>
+                    <div role="button" class="outline contrast" onclick="copyToClipboard(event, 'patchworkStaticLink')">Copy</div>
+                </div>
             </div>
         </article>
     </main>
     <footer>
         <div>made with <span class="lov3">
-                <3</span>
+                <3< /span>
         </div>
         <div>by <a href="https://artz.dev" target="_blank" rel="noopener noreferrer">Jee</a></div>
-                <a class="outline contrast" href="https://github.com/jee-r/Patchwork" role="">
-                    Fork Me
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M2.6 10.59L8.38 4.8l1.69 1.7c-.24.85.15 1.78.93 2.23v5.54c-.6.34-1 .99-1 1.73a2 2 0 0 0 2 2a2 2 0 0 0 2-2c0-.74-.4-1.39-1-1.73V9.41l2.07 2.09c-.07.15-.07.32-.07.5a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2c-.18 0-.35 0-.5.07L13.93 7.5a1.98 1.98 0 0 0-1.15-2.34c-.43-.16-.88-.2-1.28-.09L9.8 3.38l.79-.78c.78-.79 2.04-.79 2.82 0l7.99 7.99c.79.78.79 2.04 0 2.82l-7.99 7.99c-.78.79-2.04.79-2.82 0L2.6 13.41c-.79-.78-.79-2.04 0-2.82Z" />
-                    </svg>
-                </a>
+        <a class="outline contrast" href="https://github.com/jee-r/Patchwork" role="">
+            Fork Me
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M2.6 10.59L8.38 4.8l1.69 1.7c-.24.85.15 1.78.93 2.23v5.54c-.6.34-1 .99-1 1.73a2 2 0 0 0 2 2a2 2 0 0 0 2-2c0-.74-.4-1.39-1-1.73V9.41l2.07 2.09c-.07.15-.07.32-.07.5a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2c-.18 0-.35 0-.5.07L13.93 7.5a1.98 1.98 0 0 0-1.15-2.34c-.43-.16-.88-.2-1.28-.09L9.8 3.38l.79-.78c.78-.79 2.04-.79 2.82 0l7.99 7.99c.79.78.79 2.04 0 2.82l-7.99 7.99c-.78.79-2.04.79-2.82 0L2.6 13.41c-.79-.78-.79-2.04 0-2.82Z" />
+            </svg>
+        </a>
     </footer>
 </body>
 
